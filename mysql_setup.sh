b359004faad110cf29dfbd6e51457e1f3fbc8aec
aliahad97 <ali_thespy@hotmail.com>
2020-06-27 09:07:11 -0700

    Fixed mysql_setup.sh

--- mysql_setup.sh
+++ mysql_setup.sh
@@ -1,14 +1,22 @@
 echo "Setup wllvm environemnt"
 source wllvm.env
-# Preconfiguration setup
-groupadd mysql
-useradd -r -g mysql -s /bin/false mysql
+# Preconfiguration setup. The below steps are commented out as they can be later run when installing. Building in no way require these steps.
 
-cd mysql-installer/mysql-5.7-5.7.29
+# groupadd mysql
+# useradd -r -g mysql -s /bin/false mysql
+
+cd mysql-5.7-5.7.29
 mkdir bld
 cd bld
 
 CC=wllvm CXX=wllvm++ cmake --build ../  --target sqld
 
 make 
+
+# Go back to original directory
+cd ../../
+
+# Installation can be done by running the following command in the "bld" directory
+
+# cd mysql-5.7-5.7.29/bld
 # make install
